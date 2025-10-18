174bdff2184b12d88067a405bbddd692b5a49cc9
keyboard <jom184750@gmail.com>
2025-10-18 09:46:45 +0300

    404 added

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -2,7 +2,7 @@
 
 
 // import React from 'react'
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 // const NotFound = () => {
 //   return (
@@ -18,9 +18,9 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return (
       <div style={{ textAlign: 'center', margin: '50px' }}>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        <Link href="/">Go back to Home</Link>
+        <h1>404 - Page Not Found 444444444000000000000000000000444444444444444444444444444444</h1>
+        <p>The page you are looking for does not exist 44444444444444444444.</p>
+        {/* <Link href="/">Go back to Home</Link> */}
       </div>
     );
   };
